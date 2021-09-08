825509560d25c4631756329cdb5bd31d459637b0
mauricio-molina-git <81722575+mauricio-molina-git@users.noreply.github.com>
2021-09-08 14:24:56 -0300

    Activity de registro, y inicie el navigation drawer.

---
+++ app/src/main/java/com/example/tp3_controlesyalmacenamientosqllite/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.tp3_controlesyalmacenamientosqllite.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
