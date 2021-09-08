507bc74ca9140371b4feb71a705abdf76d7224bf
JRodriguez-1998 <jose.rodriguez@alumnos.frgp.utn.edu.ar>
2021-09-07 23:10:44 -0300

    Creación Proyecto. Splash y Activity Principal

---
+++ app/src/main/java/com/example/tp3_controlesyalmacenamientosqllite/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.tp3_controlesyalmacenamientosqllite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =  new Intent(MainActivity.this, MenuPrincipal.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+    }
+}
